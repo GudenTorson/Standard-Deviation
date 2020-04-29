edb7c987c9c3dd7c2588e1778b825b1fe4c43ad9
Tor Smedberg <tor.smedberg@gmail.com>
2020-04-29 18:04:49 +0200

    Created Solution

---
+++ Standard Deviation/Standard Deviation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Standard_Deviation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
