26b6bc659cb9735ddb841cc72cc98be0afa7a8ba
Tor Smedberg <tor.smedberg@gmail.com>
2020-04-30 22:14:28 +0200

    added most of the calculating functions and added argument parsing.

--- Standard Deviation/Standard Deviation/Program.cs
+++ Standard Deviation/Standard Deviation/Program.cs
@@ -10,12 +10,25 @@ namespace Standard_Deviation
     {
         static void Main(string[] args)
         {
-            if (args[0] != "-i" || args[0] != "-i")
-            { 
-            Console.WriteLine("Enter all your values. If a value has a frequency more than 1, please enter it in the form of VALUE*FREQUENCY. Separate values with a comma','. Spaces are not needed. When all values have been entered hit ENTER.");
-            string input = Console.ReadLine();
-            listValues(input);
+            if (args[0] == "-I" || args[0] == "-i")
+            {
+                Console.WriteLine("Trying to use supplied arguments...");
+                parseInput(args[1]);
             }
+            else if (args[0] == "")
+            {
+                Console.WriteLine("Enter all your values. If a value has a frequency more than 1, please enter it in the form of VALUE*FREQUENCY. Separate values with a comma','. Spaces are not needed. When all values have been entered hit ENTER.");
+                string input = Console.ReadLine();
+                parseInput(input);
+
+            }
+            else if (args[0] != "")
+            {
+                Console.WriteLine("Unknown argument(s) given, here is a list of the syntax.");
+                Console.WriteLine("Standard-Deviation.exe -i \"value1,value2,value3...\"");
+            
+            }
+
 
         }
         
@@ -23,6 +36,33 @@ namespace Standard_Deviation
         {
             //Enters all the input values into the global list
             
+        }
+
+        static int listSum()
+        {
+            int sum = 0;
+            for (int i = 0; i == values.count; i++)
+            {
+                sum += values[i];
+            }
+            return sum;
+        }
+
+        static int listAverage()
+        {
+            return listSum / values.count;
+        }
+
+        static int standardDeviation()
+        {
+            // Calculates the sum and then the standard deviation
+            int workingInt = 0;
+            int average = listAverage();
+            for (int i = 0; i == values.count; i++)
+            {
+                workingInt += Math.Pow((values[i] - average), 2);
+            }
+            return Math.Sqrt((workingInt/(values.count - 1)));
         }
     }
 }
