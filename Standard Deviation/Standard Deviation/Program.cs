a5e025401418407de5214e93932adc17ad7b50e4
isilinh02 <64552458+ellioth02@users.noreply.github.com>
2020-04-30 22:14:21 +0200

    Fixed list and functioning parser without error handling

--- Standard Deviation/Standard Deviation/Program.cs
+++ Standard Deviation/Standard Deviation/Program.cs
@@ -8,18 +8,50 @@ namespace Standard_Deviation
 {
     class Program
     {
-        List<double> values = new List<double>();
+        static List<double> values = new List<double>();
         static void Main(string[] args)
         {
             Console.WriteLine("Enter all your values. If a value has a frequency more than 1, please enter it in the form of VALUE*FREQUENCY. Separate values with a comma','. Spaces are not needed. When all values have been entered hit ENTER.");
             string input = Console.ReadLine();
             parseInput(input);
+
+            for (int i = 0; i < values.Count; i++)
+            {
+                Console.WriteLine(values[i]);
+            }
+
+            Console.ReadKey();
         }
         
         static void parseInput(string input)
         {
             //Enters all the input values into the global list
-            
+            string[] items = input.Split(',');
+            for (int i = 0; i < items.Length; i++)
+            {
+                parseItem(items[i]);
+            }
+        }
+
+        static void parseItem(string item)
+        {
+            // check if the item contains multiple of values
+            if (item.Contains('*'))
+            {
+                string[] arguments = item.Split('*');
+                double num = double.Parse(arguments[0]);
+                int times = int.Parse(arguments[1]);
+
+                for (int i = 0; i < times; i++)
+                {
+                    values.Add(num);
+                }
+            }
+            else
+            {
+                double num = double.Parse(item);
+                values.Add(num);
+            }
         }
     }
 }
